e824c7a3a465b5bab8c1405248b6c4166960333d
Thomas Snow <thomas.snow@realestate.com.au>
2017-10-18 16:22:17 +1100

    Refactor page updated

--- ruby/refactor.rb
+++ ruby/refactor.rb
@@ -74,3 +74,18 @@ class FizzBuzz
      .map { |x| x % 5 == 0 ? 'Buzz' : x }
   end
 end
+
+# --------------------------------------------------------------------
+# RANGE
+class FizzBuzz
+  def self.call(x, y)
+    a = (x..y).to_a
+    replaceNumWithString(a)
+  end
+
+  def self.replaceNumWithString(a)
+    a.map { |x| x % 3 == 0 && x % 5 == 0 ? 'FizzBuzz' : x }
+     .map { |x| x % 3 == 0 ? 'Fizz' : x }
+     .map { |x| x % 5 == 0 ? 'Buzz' : x }
+  end
+end
