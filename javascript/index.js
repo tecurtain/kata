67a2f693b185b125ecfcab21e31061800857a3b9
Thomas Snow <thomas.snow@realestate.com.au>
2017-10-23 13:34:47 +1100

    First working soultion 1 though 5, tests passing

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,40 @@
-let romanNumerals = (numbers) => {
-  return "I"
+let arabicToRoman = (value) => {
+  var numTable = {
+    "I"   : 1,
+    "II"  : 2,
+    "III" : 3,
+    "IV"  : 4,
+    "V"   : 5
+  };
+
+  var romanNum = getKeyByValue(numTable, value)
+  return romanNum
 }
 
+function getKeyByValue(hash, value) {
+  var key;
+  for(key in hash) {
+    if (hash[key] == value) return key;
+  }
+}
 
+export {arabicToRoman}
 
-export {romanNumerals}
+// 1 = I
+// 2 = II
+// 3 = III
+// 4 = IV
+// 5 = V
+// 6 = VI
+// 7 = VII
+// 8 = VIII
+// 9 = IX
+// 10 = X
+//
+// I	= 1
+// V	= 5
+// X	= 10
+// L	= 50
+// C	= 100
+// D	= 500
+// M	= 1,000
--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,14 +1,39 @@
 import chai from "chai"
-import {romanNumerals} from "../index"
+import {arabicToRoman} from "../index"
 
 
 let expect = chai.expect
 
-describe("romanNumerals", () => {
+describe("arabicToRoman", () => {
   it("returns roman number I for 1", () => {
-    expect(romanNumerals(1)).to.include("I")
+    expect(arabicToRoman(1)).to.include("I")
   })
 })
+
+describe("arabicToRoman", () => {
+  it("returns roman number II for 2", () => {
+    expect(arabicToRoman(2)).to.include("II")
+  })
+})
+
+describe("arabicToRoman", () => {
+  it("returns roman number III for 3", () => {
+    expect(arabicToRoman(3)).to.include("III")
+  })
+})
+
+describe("arabicToRoman", () => {
+  it("returns roman number IV for 4", () => {
+    expect(arabicToRoman(4)).to.include("IV")
+  })
+})
+
+describe("arabicToRoman", () => {
+  it("returns roman number V for 5", () => {
+    expect(arabicToRoman(5)).to.include("V")
+  })
+})
+
 
 // In the Roman Numerals kata, we convert Arabic numbers
 // (the one we use daily: 1, 2, 3, 4, 5, …)
