99c025933cfd3e7a4c932b1440e4e8e3a9c1635a
Thomas Snow <thomas.snow@realestate.com.au>
2017-10-27 15:48:37 +1100

    Working ruby solution and tests, similar to JS solution

--- ruby/lib/arabic_to_roman.rb
+++ ruby/lib/arabic_to_roman.rb
@@ -1,7 +1,31 @@
 require "pry"
 
 class ArabicToRoman
-  def self.call(number)
-    "I"
+
+  NUMERALS = [
+      ["M", 1000],
+      ["CM", 900],
+      ["D", 500],
+      ["CD", 400],
+      ["C", 100],
+      ["XC", 90],
+      ["L", 50],
+      ["XL", 40],
+      ["X", 10],
+      ["IX", 9],
+      ["V", 5],
+      ["IV", 4],
+      ["I", 1]
+  ]
+
+    def self.call(number)
+    result = ""
+    NUMERALS.each do |romanNumeral, decimalValue|
+      while number >= decimalValue
+        result += romanNumeral
+        number -= decimalValue
+      end
+    end
+    result
   end
 end
--- ruby/spec/roman_numeral_spec.rb
+++ ruby/spec/roman_numeral_spec.rb
@@ -10,9 +10,34 @@ RSpec.describe ArabicToRoman do
   it "returns III for 3" do
     expect(ArabicToRoman.call(3)).to eq "III"
   end
+  it "returns IV for 4" do
+    expect(ArabicToRoman.call(4)).to eq "IV"
+  end
+  it "returns V for 5" do
+    expect(ArabicToRoman.call(5)).to eq "V"
+  end
+  it "returns X for 10" do
+    expect(ArabicToRoman.call(10)).to eq "X"
+  end
+  it "returns XIV for 14" do
+    expect(ArabicToRoman.call(14)).to eq "XIV"
+  end
+  it "returns L for 50" do
+    expect(ArabicToRoman.call(50)).to eq "L"
+  end
+  it "returns CXLIV for 144" do
+    expect(ArabicToRoman.call(144)).to eq "CXLIV"
+  end
+  it "returns C for 100" do
+    expect(ArabicToRoman.call(100)).to eq "C"
+  end
+  it "returns D for 500" do
+    expect(ArabicToRoman.call(500)).to eq "D"
+  end
+  it "returns M for 1000" do
+    expect(ArabicToRoman.call(1000)).to eq "M"
+  end
+  it "returns MCXLIV for 1144" do
+    expect(ArabicToRoman.call(1144)).to eq "MCXLIV"
+  end
 end
-
-
-# In the Roman Numerals kata, we convert Arabic numbers
-# (the one we use daily: 1, 2, 3, 4, 5, …)
-# into their Roman equivalent: I, II, III, IV, V
