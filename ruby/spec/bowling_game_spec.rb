31c6bdfbfa2635a313f04d59ff7b9c2e7d7a02e1
Thomas Snow <thomas.snow@realestate.com.au>
2017-11-20 18:28:30 +1100

    Roll_many refactor

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -5,14 +5,20 @@ RSpec.describe BowlingGame do
     @game = BowlingGame.new
   end
 
+  def roll_many(n, pins)
+    n.times do
+      @game.roll(pins)
+    end
+  end
+
   it "can bowl a gutter game of all zeros" do
-    20.times { @game.roll(0) }
+    roll_many(20, 0)
 
     expect(@game.score).to eq(0)
   end
 
   it "can bowl a game of all ones" do
-     20.times { @game.roll(1) }
+     roll_many(20, 1)
 
      expect(@game.score).to eq(20)
    end
@@ -20,7 +26,7 @@ RSpec.describe BowlingGame do
   it "can bowl a game with a spare" do
     2.times { @game.roll(5) }
     @game.roll(3)
-    17.times { @game.roll(0) }
+    roll_many(17, 0)
 
     expect(@game.score).to eq(16)
   end
@@ -29,7 +35,7 @@ RSpec.describe BowlingGame do
     @game.roll(10)
     @game.roll(3)
     @game.roll(4)
-    16.times { @game.roll(0) }
+    roll_many(16, 0)
 
     expect(@game.score).to eq(24)
   end
