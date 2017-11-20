8fa486fc066ac6c17a90d26212823b2d7a518da3
Thomas Snow <thomas.snow@realestate.com.au>
2017-11-20 18:41:05 +1100

    refactored strike, spare and ball_sum in game file

--- ruby/lib/bowling_game.rb
+++ ruby/lib/bowling_game.rb
@@ -9,18 +9,30 @@ class BowlingGame
     @rolls << pins
   end
 
+  def strike?(frame_index)
+    @rolls[frame_index] == 10
+  end
+
+  def spare?(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1] == 10
+  end
+
+  def sum_of_balls_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
   def score
     score = 0
     frame_index = 0
     frame = 0
     while frame < 10
-      if @rolls[frame_index] == 10
+      if strike?(frame_index)
         score += 10 + @rolls[frame_index + 1] + @rolls[frame_index + 2]
         frame_index += 1
-    elsif @rolls[frame_index] + @rolls[frame_index + 1] == 10
+    elsif spare?(frame_index)
       score += 10 + @rolls[frame_index + 2]
       frame_index += 2
-    else score += @rolls[frame_index] + @rolls[frame_index + 1]
+    else score += sum_of_balls_in_frame(frame_index)
         frame_index += 2
       end
       frame += 1
