40902dd30ff0820460a2a69f168679df31ea37e0
Thomas Snow <thomas.snow@realestate.com.au>
2017-12-11 11:37:44 +1100

    Code updated, all new tests passing

--- ruby/lib/advent_day_one.rb
+++ ruby/lib/advent_day_one.rb
@@ -3,32 +3,20 @@ require 'pry'
 class Captcha
 
   def initialize
-    @answer = []
+    @input_array = []
     @sum_array = [0]
   end
 
   def to_array(input)
-    @answer = input.each_char.map(&:to_i)
+    @input_array = input.each_char.map(&:to_i)
   end
 
-  def calculate_next_element
+  def calculate_first_mid_element
     array_index = 0
-    while array_index < @answer.length
-      if @answer[array_index] == @answer[array_index + 1]
-        @sum_array << @answer[array_index]
-        array_index += 1
-      else
-        array_index += 1
-      end
-    end
-  end
-
-  def calculate_first_final_element
-    array_index = 0
-    array_end = @answer.length - 1
-    while array_index < @answer.length
-      if @answer[array_index] == @answer[array_index + array_end]
-        @sum_array << @answer[array_index + array_end]
+    mid_array_index = @input_array.length / 2
+    while array_index < @input_array.length
+      if @input_array[array_index] == @input_array[array_index + mid_array_index]
+        @sum_array << @input_array[array_index] + @input_array[array_index + mid_array_index]
         array_index += 1
       else
         array_index += 1
@@ -37,18 +25,7 @@ class Captcha
   end
 
   def calculate_total
-    calculate_next_element
-    calculate_first_final_element
+    calculate_first_mid_element
     @sum_array.reduce(:+)
   end
 end
-
-# def answer
-#   val = 0
-#   @answer.each_with_index do |val, index|
-#     while val(index) == val(index + 1)
-#     @answer << val
-#     @answer
-#     end
-#   end
-# end
