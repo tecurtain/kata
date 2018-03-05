d51c86b26648ee0452e6bae239858e302d794320
Thomas Snow <thomas.snow@realestate.com.au>
2018-03-05 11:29:06 +1100

    WIP

--- javascript/index.js
+++ javascript/index.js
@@ -8,24 +8,14 @@ export function filter(inputArray, filterFunc) {
   var returnArray = []
   //declares a variable called currentIndex and sets the value to 0 to later track which element in the return array is currently being refered to
   var currentIndex = 0
-  //declares a function that takes two arguments, the current element in a given array, and the current index of the array
-  function filterFunc(currentElement) {
-    // If the currentElement is true
-    if (currentElement) {
-      // filterFunction will return the currentElement
-      return currentElement
-      //if it is anything but true it will return false
-    } else {
-      return false
-    }
-  }
   //while statement will create a loop that will run only when the conditionanl statment in the () are met.  The following code block will excute as many times as the conditional statment is true.  While the currentIndex is less than the inputArrayLength the flowing code block will execute.
   while (currentIndex < inputArrayLength) {
     //declares a variable currentElement that assigns the value of the element in a specific location of the inputArray base on the value of the currentIndex
     var currentElement = inputArray[currentIndex]
     // declares an if statment that when calling filterFunc and passing the currentElement evalutes to true it will push the currentElement to the inputArray and increment the currentIndex by 1. If the statment evalues to false it will increment the currentIndex by 1.
-    if (filterFunc(currentElement)) {
-      returnArray.push(newElement)
+    if (filterFunc(inputArray)) {
+      console.log(inputArray)
+      returnArray.push(currentElement)
       currentIndex++
     } else {
       currentIndex++
